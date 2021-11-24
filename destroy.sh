c84b69594909a3c7c8019670ff5a6d52bb16bc22
EC2 Default User <ec2-user@ip-192-168-50-167.sa-east-1.compute.internal>
2021-11-24 12:16:59 +0000

    add checagem no destroy

--- destroy.sh
+++ destroy.sh
@@ -9,7 +9,14 @@ AMIID=`cat /tmp/k8s-ami-id.tmp`
 cd $TFPATH/0-terraform/
 mv sg-ok.tf $TFPATH/tmp/sg-ok.tf.bkp
 cp sg-nok.tf.bkp sg-nok.tf
-terraform init
-TF_VAR_amiid=$AMIID terraform apply -auto-approve
+
+CHKTFOUTPUT=$(terraform output | wc -l)
+
+if [ ${CHKTFOUTPUT} -lt 26 ]; 
+  then
+  terraform init
+  TF_VAR_amiid=$AMIID terraform apply -auto-approve
+fi
+
 sleep 5
 TF_VAR_amiid=$AMIID terraform destroy -auto-approve
